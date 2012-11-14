20fd3cb1a84eda652977519c323baf7617d86976
Shaun Pack <kaizenpack@gmail.com>
2012-11-14 13:43:04 -0500

    initial import after rails new twitdocs --skip-active-record

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitdocs::Application.config.secret_token = 'b50c0921d794c27e77e2ffbcec620bc91074b1fb2543e9702a3a2c1634a070d68ef6dc0c741cdacd99a5dfa34871d35914a718e41ec8729b14211addb36b7c49'
